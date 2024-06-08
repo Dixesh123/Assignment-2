a5dd32267b3f71288415961784311cf416dc2b55
Dixesh123 <dixoza4667@gmail.com>
2024-06-08 14:05:06 -0400

    First commit
    Initial commit

---
+++ Assignment2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GemHunters
+{
+    class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
