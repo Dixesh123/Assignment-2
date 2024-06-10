b8bd502038090a4b2650f4776fd4c0877c339a90
Dixesh123 <dixoza4667@gmail.com>
2024-06-10 17:59:47 -0400

    Display Method: Prints the current state of the board to the console. IsValidMove Method: Checks if a move in a specified direction for a given player is valid. CollectGem Method: Allows a player to collect a gem if present at their current position on the board.
    1.Method of Display:
    
    -Goal: Prints the board's current status
     to the console.
    
    2.Is Valid Move Approach:
    
    -Determines if a move made by a
     certain player in a given direction is
     permissible.
    
    3.CollectGem Approach:
    
    -Goal: If a gem is present in the
     player's current position on the board,
     it enables them to collect it.

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -88,8 +88,49 @@ namespace GemHunters
                 }
             }
 
-        }
+            public void Display()
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        Console.Write(Grid[i, j].Occupant + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
 
+            public bool IsValidMove(Player player, char direction)
+            {
+                int newX = player.Position.X;
+                int newY = player.Position.Y;
+
+                switch (direction)
+                {
+                    case 'U': newY--; break;
+                    case 'D': newY++; break;
+                    case 'L': newX--; break;
+                    case 'R': newX++; break;
+                }
+
+                if (newX < 0 || newX >= 6 || newY < 0 || newY >= 6)
+                    return false;
+                if (Grid[newY, newX].Occupant == "O")
+                    return false;
+
+                return true;
+            }
+
+            public void CollectGem(Player player)
+            {
+                if (Grid[player.Position.Y, player.Position.X].Occupant == "G")
+                {
+                    player.GemCount++;
+                    Grid[player.Position.Y, player.Position.X].Occupant = "-";
+                }
+            }
+        }
     }
-}
+  }
+ 
 
